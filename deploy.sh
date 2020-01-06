f0ddc245367662da3cf25c1a758fd09982e410ac
Mateusz Krawczyk <krawat10@gmail.com>
2020-01-06 16:07:20 +0100

    fix2

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -x #echo on
-sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y python3
-sudo apt-get install -y python3-pip
+sudo apt-get install --yes --force-yes --keep-current-confs python3
+sudo apt-get install --yes --force-yes --keep-current-confs python3-pip
 pip3 install django
 pip3 install virtualenv
 pip3 install -r requirements.txt
