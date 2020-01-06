cd9fd25fe557ef19240dee3832a397d36599230d
Mateusz Krawczyk <krawat10@gmail.com>
2020-01-06 03:12:55 +0100

    sh changes3

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+set -x #echo on
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install python3
 sudo apt-get install -y python3-pip
 pip3 install django
 pip3 install virtualenv
-pip3 install -r -y requirements.txt
+pip3 install -r requirements.txt
 virtualenv env
 . env/bin/activate
 python3 manage.py runserver 8000
