cf5263fc27c7d9696262778e68c8dd8994bb07db
Mateusz Krawczyk <krawat10@gmail.com>
2020-01-20 22:26:03 +0100

    restore 80 port

--- NewProject/EthernetSwitch/EthernetSwitch/Program.cs
+++ NewProject/EthernetSwitch/EthernetSwitch/Program.cs
@@ -20,6 +20,7 @@ namespace EthernetSwitch
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:80");
                     webBuilder.UseStartup<Startup>();
                 });
     }
