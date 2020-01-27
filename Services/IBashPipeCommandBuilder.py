6f46a421139a25a528502764877292d9ac6d2674
Mateusz Krawczyk <krawat10@gmail.com>
2020-01-28 00:01:49 +0100

    Rmoved and frefactoring projecyt

--- Services/IBashPipeCommandBuilder.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from abc import ABC, abstractmethod
-from typing import Tuple
-
-
-class IBashPipeCommandBuilder(ABC):
-    @abstractmethod
-    def execute(self, cmd: str) -> Tuple[str, str, int]: raise NotImplementedError
--- switch/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SwitchConfig(AppConfig):
-    name = 'switch'
