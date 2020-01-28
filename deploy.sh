34a4240917c7bd48ee8dfeeadd41e396657f0950
Mateusz Krawczyk <krawat10@gmail.com>
2020-01-28 21:05:17 +0100

    added deploy.sh file

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y aptitude
+aptitude install -y tcpdump
+aptitude install -y bridge-utils
+aptitude install -y vlan
+aptitude install -y tmux
+aptitude install -y git
+sudo apt install -y gnupg
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/debian/10/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y aspnetcore-runtime-3.1
+sudo apt-get update
+sudo apt-get install -y dotnet-runtime-3.1
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-3.1
+
+sudo dotnet publish -c Release ./EthernetSwitch
+cd ./EthernetSwitch/bin/Release/netcoreapp3.1/
+sudo dotnet EthernetSwitch.dll
