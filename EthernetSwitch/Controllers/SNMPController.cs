19aeb6e64294f2362d98589de7701717d29032b1
Mateusz Krawczyk <krawat10@gmail.com>
2020-10-12 23:03:17 +0200

    Added Set SNMP Response

--- EthernetSwitch/Controllers/SNMPController.cs
+++ EthernetSwitch/Controllers/SNMPController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using EthernetSwitch.Models.SNMP;
 using Lextm.SharpSnmpLib;
 using Lextm.SharpSnmpLib.Messaging;
@@ -15,12 +14,18 @@ namespace EthernetSwitch.Controllers
     {
         public IActionResult GetSNMPv3()
         {
-            return View(new GetSNMPv3ViewModel { IpAddress = HttpContext.Connection.LocalIpAddress.ToString() });
+            return View(new GetSNMPv3ViewModel {IpAddress = HttpContext.Connection.LocalIpAddress.ToString()});
         }
+
+        public IActionResult SetSNMPv3()
+        {
+            return View(new SetSNMPv3ViewModel { IpAddress = HttpContext.Connection.LocalIpAddress.ToString() });
+        }
+
 
         public IActionResult WalkSNMPv1()
         {
-            return View(new WalkSNMPv1ViewModel { IpAddress = HttpContext.Connection.LocalIpAddress.ToString() });
+            return View(new WalkSNMPv1ViewModel {IpAddress = HttpContext.Connection.LocalIpAddress.ToString()});
         }
 
         [HttpPost]
@@ -39,24 +44,17 @@ namespace EthernetSwitch.Controllers
                     10000,
                     WalkMode.WithinSubtree);
             }
-            catch (System.Exception e)
+            catch (Exception e)
             {
                 viewModel.Error = e.Message;
             }
 
             viewModel.OIDs = result
-                .Select(variable => new OID { Id = variable.Id.ToString(), Value = variable.Data.ToString() })
+                .Select(variable => new OID {Id = variable.Id.ToString(), Value = variable.Data.ToString()})
                 .ToList();
 
             return View("WalkSNMPv1", viewModel);
         }
-        //
-        // [HttpPost]
-        // public IActionResult TrapSNMPv3(GetSNMPv3ViewModel viewModel)
-        // {
-        //     
-        // }
-
 
         [HttpPost]
         public IActionResult GetSNMPv3(GetSNMPv3ViewModel viewModel)
@@ -74,14 +72,14 @@ namespace EthernetSwitch.Controllers
                     Messenger.NextMessageId,
                     Messenger.NextRequestId,
                     new OctetString(viewModel.UserName),
-                    new List<Variable> { new Variable(new ObjectIdentifier(viewModel.OID.Id)) },
+                    new List<Variable> {new Variable(new ObjectIdentifier(viewModel.OID.Id))},
                     new DESPrivacyProvider(
                         new OctetString(viewModel.Encryption),
                         new MD5AuthenticationProvider(new OctetString(viewModel.Password))
                     ),
                     Messenger.MaxMessageSize,
                     report);
-                
+
                 ISnmpMessage reply = request.GetResponse(10000, new IPEndPoint(ipAddress, 161));
 
                 var valiable = reply
@@ -97,14 +95,51 @@ namespace EthernetSwitch.Controllers
                 {
                     throw ErrorException.Create("error in response", ipAddress, reply);
                 }
-
             }
-            catch (System.Exception e)
+            catch (Exception e)
             {
                 viewModel.Error = e.Message;
             }
 
             return View("GetSNMPv3", viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult SetSNMPv3(SetSNMPv3ViewModel viewModel)
+        {
+            var ipAddress = IPAddress.Parse(viewModel.IpAddress);
+            viewModel.Error = "";
+
+            try
+            {
+                Discovery discovery = Messenger.GetNextDiscovery(SnmpType.GetRequestPdu);
+                ReportMessage report = discovery.GetResponse(10000, new IPEndPoint(ipAddress, viewModel.Port));
+
+                SetRequestMessage request = new SetRequestMessage(
+                    VersionCode.V3,
+                    Messenger.NextMessageId,
+                    Messenger.NextRequestId,
+                    new OctetString(viewModel.UserName),
+                    new List<Variable> {new Variable(new ObjectIdentifier(viewModel.OID.Id))},
+                    new DESPrivacyProvider(
+                        new OctetString(viewModel.Encryption),
+                        new MD5AuthenticationProvider(new OctetString(viewModel.Password))
+                    ),
+                    report);
+
+                ISnmpMessage reply = request.GetResponse(10000, new IPEndPoint(ipAddress, 161));
+
+                if (reply.Pdu().ErrorStatus.ToInt32() != 0) // != ErrorCode.NoError
+                {
+                    throw ErrorException.Create("error in response", ipAddress, reply);
+                }
+            }
+            catch (Exception e)
+            {
+                viewModel.Error = e.Message;
+            }
+
+            return View("SetSNMPv3", viewModel);
         }
     }
 }
--- EthernetSwitch/Models/SNMP/SetSNMPv3ViewModel.cs
+++ EthernetSwitch/Models/SNMP/SetSNMPv3ViewModel.cs
@@ -3,22 +3,15 @@ using Lextm.SharpSnmpLib;
 
 namespace EthernetSwitch.Models.SNMP
 {
-    public class OID
-    {
-        public string Id { get; set; }
-        public string Value { get; set; }
-    }
-
-
-    public class GetSNMPv3ViewModel
+    public class SetSNMPv3ViewModel
     {
         public string UserName { get; set; }
         public VersionCode VersionCode { get; set; } = VersionCode.V3;
         public string IpAddress { get; set; } = "127.0.0.1";
         public int Port { get; set; } = 161;
-        [DataType(DataType.Password)]public string Password { get; set; }
-        [DataType(DataType.Password)]public string Encryption { get; set; }
+        [DataType(DataType.Password)] public string Password { get; set; }
+        [DataType(DataType.Password)] public string Encryption { get; set; }
         public OID OID { get; set; }
-        public string Error { get;  set; }
+        public string Error { get; set; }
     }
 }
