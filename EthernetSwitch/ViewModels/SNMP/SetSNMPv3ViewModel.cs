80ffbd032d4b5c397cfdddefa75d97a037b1c989
krawat10 <krawat10@gmail.com>
2020-12-04 16:29:33 +0100

    Added validation

--- EthernetSwitch/ViewModels/SNMP/SetSNMPv3ViewModel.cs
+++ EthernetSwitch/ViewModels/SNMP/SetSNMPv3ViewModel.cs
@@ -6,14 +6,18 @@ namespace EthernetSwitch.Models.SNMP
 {
     public class SetSNMPv3ViewModel
     {
+        [Required]
         public string UserName { get; set; }
         public VersionCode VersionCode { get; set; } = VersionCode.V3;
+        [Required]
         public string IpAddress { get; set; } = "127.0.0.1";
+        [Required]
         public int Port { get; set; } = 161;
-        [DataType(DataType.Password)] public string Password { get; set; }
-        [DataType(DataType.Password)] public string Encryption { get; set; }
-        public EncryptionType EncryptionType { get; set; }
-        public OID OID { get; set; }
+        [DataType(DataType.Password)][Required] public string Password { get; set; }
+        [DataType(DataType.Password)][Required] public string Encryption { get; set; }
+        [Required] public EncryptionType EncryptionType { get; set; }
+        [Required]public OID OID { get; set; }
         public string Error { get; set; }
+        public bool Success { get; set; }
     }
 }
