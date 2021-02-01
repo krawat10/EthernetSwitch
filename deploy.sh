a51e72f58a62dbb24478dbc1a52538602ad24b0a
Mateusz Krawczyk <krawat10@gmail.com>
2021-02-01 18:32:18 +0100

    Updated instructions

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,42 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get install -y aptitude
-aptitude install -y tcpdump
-aptitude install -y bridge-utils
-aptitude install -y vlan
-aptitude install -y tmux
-aptitude install -y git
-sudo apt install -y gnupg
-
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
-sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
-wget -q https://packages.microsoft.com/config/debian/10/prod.list
-sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
-sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
-sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
-
+#!/bin/bash
 sudo apt-get update
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y aspnetcore-runtime-3.1
-sudo apt-get update
+
+wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-3.1
+
 sudo apt-get install -y dotnet-runtime-3.1
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-3.1
-sudo apt-get update
-sudo apt-get install -y libpcap-dev
 
-sudo dotnet publish -c Release ./EthernetSwitch
-cd ./EthernetSwitch/bin/Release/netcoreapp3.1/publish/
-sudo dotnet EthernetSwitch.dll
+
+sudo apt-get install -y aptitude
+sudo aptitude install -y tcpdump
+sudo aptitude install -y bridge-utils
+sudo aptitude install -y vlan
+sudo aptitude install -y tmux
+sudo aptitude install -y git
+sudo aptitude install -y gnupg
+sudo aptitude install snmpd snmp libsnmp-dev -y
+
+#SNMP
+sudo systemctl start snmpd
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo git clone --depth 1 https://github.com/krawat10/EthernetSwitch.git
+sudo dotnet publish -c Release EthernetSwitch/EthernetSwitch
+
+dir=$`pwd`/
+sudo sed -i 's/PATH/${dir//\//\\/}/g' EthernetSwitch/EthernetSwitch/ethernet-switch.service
+sudo cp EthernetSwitch/EthernetSwitch/ethernet-switch.service /etc/systemd/system/ethernet-switch.service
+sudo systemctl daemon-reload
+sudo systemctl enable ethernet-switch.service
+sudo systemctl start ethernet-switch.service
